a3767705b3a1cda81490917e3b2d26eb69057a7a
jahigd02 <jahigd02@louisville.edu>
2022-12-21 02:20:41 -0500

    Character lookup

--- source/bookj.c
+++ source/bookj.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 
+int peek(FILE *fp, int index, char expected);
+int find(FILE *fp, char* character_name);
+char* retrieve_name(FILE *fp, int index);
+
 int peek(FILE *fp, int index, char expected) {
   char c = fgetc(fp);
   if (c == expected) {
@@ -13,36 +17,42 @@ int peek(FILE *fp, int index, char expected) {
 
 int find(FILE *fp, char* character_name) {
   int index = 0;
-  char c; 
+  char c;
+  
   while ((c = fgetc(fp)) != EOF) {
-    if (c == character_name[0]) {
-      if (peek(fp, index, character_name[1]) == 1) {
-	index++;
+    // fgetc() will consume the character at index N, so the fp is really pointing to index + 1 after the call.
+    // as a result, retrieve_name will start at the character after !, not at the first letter of the Character's name.
+    // so, in retrieve_name, we add 1 to index to point it to the first letter of the Character's name. fseek() brings the fp there.
+    if (c == '!' && peek(fp, index, 'C') == 1) {
+      if (strcmp(retrieve_name(fp, index), character_name) == 0) {
+	printf("Character %s found at index %d\n", character_name, index);
 	return index;
-      } else {	
-	c = fgetc(fp);
       }
     }
     index++;
   }
-
+  printf("Character %s not found.", character_name);
   return -1;
 }
 
-char* retrieve_name(FILE *fp, int index, char* name) {
+char* retrieve_name(FILE *fp, int index) {
   int length = 0;
   char c;
-
-  fseek(fp, index+1, SEEK_SET);
+  index = index + 1;
+  fseek(fp, index, SEEK_SET);
   
   while ((c = fgetc(fp)) != '!' && (c != EOF)) {
-    length++;
+    printf("%c", c);
+    if (c != '\n')
+      length++;
   }
 
-  fseek(fp, index+1, SEEK_SET);
+  fseek(fp, index, SEEK_SET);
   
   char* name = malloc(sizeof(char) * length);
   fread(name, sizeof(char), length, fp);
+
+  fseek(fp, index - 1, SEEK_SET);
 
   return name;
 }
@@ -57,8 +67,8 @@ int main (int argc, char* argv[]) {
     exit(-1);
   }
 
-  int index = find(fp, "!C");
-  retrieve_name(fp, index);
+  int index = find(fp, "Anjin");
+  //  retrieve_name(fp, index, "Toranaga");
   //  printf("Length: %d", length);
   
   fclose(fp);
